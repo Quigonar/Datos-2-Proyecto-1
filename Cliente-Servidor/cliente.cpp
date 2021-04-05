509253f2d5954380e00b985ecc1898680b10ffcb
Quigonar <67380073+Quigonar@users.noreply.github.com>
2021-04-04 19:26:08 -0600

    Nuevo servidor y cliente

--- Cliente-Servidor/cliente.cpp
+++ Cliente-Servidor/cliente.cpp
@@ -1,23 +1,48 @@
 #include <SFML/Network.hpp>
 #include <SFML/Graphics.hpp>
+#include "rapidjson/document.h"
 #include <string>
 #include <iostream>
 
 using namespace sf;
 using namespace std;
+using namespace rapidjson;
+
+Document jsonReceiver(Packet packet)
+{
+    string pet;
+    Document petD;
+
+    packet >> pet;
+    cout << pet << endl;
+    const char* petChar = pet.c_str();
+    petD.Parse(petChar);
+
+    return petD;
+}
+
+string jsonSender(string type, string value, string variable, string line)
+{
+    string jsonStr = "{\"type\":"+ type + ",\"value\":" + value + ",\"variable\":" + variable + ",\"line\":" + line + "}";
+    return jsonStr;
+}
 
 int main()
 {
     IpAddress ip = IpAddress::getLocalAddress();
     TcpSocket socket;
-    char connectionType, mode, buffer[2000];
+    char connectionType, mode;
     size_t received;
-    socket.connect(ip, 8080);
-    cout << "Connected to server" << endl;
+    Document RLV;
+    Packet packetS, packetR;
+    string json;
 
-    RenderWindow window(VideoMode(800,600), "Servidor");
+    socket.connect(ip, 8080);
+
+    RenderWindow window(VideoMode(1000,800), "C! IDE");
 
     socket.setBlocking(false);
+    mode = 's';
 
     while(window.isOpen())
     {
@@ -27,17 +52,37 @@ int main()
             if (event.type == Event::Closed || event.key.code == Keyboard::Escape)
                 window.close();
         }
-        Packet packet;
+
+        json = jsonSender("int", "20", "a", "1");
+        packetS << json;
         
+        if (mode == 's')
+        {
+            if (socket.send(packetS))
+            {
+                cout << "I will now receive a message" << endl;
+                mode = 'r';
+            }
+            else
+                cout << "Message hasn't been sent" << endl;
+        }
+        else if (mode == 'r')
+        {
+            socket.receive(packetR);
+            if (packetR.getData() == NULL)
+                cout << "No message received yet" << endl;
+                
+            else
+            {
+                cout << packetR.getData() << endl;
+                RLV = jsonReceiver(packetR);
+                mode = 's';
+            }
+        }
 
         window.display();
         window.clear();
     }
-
-    //socket.send(text.c_str(), text.length() + 1);
-    //socket.receive(buffer, sizeof(buffer), received);
-    //cout << buffer << endl;
     
-    system("pause");
     return 0;
 }
--- Cliente-Servidor/servidor.cpp
+++ Cliente-Servidor/servidor.cpp
@@ -1,26 +1,100 @@
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+#include "rapidjson/document.h"
 #include <string>
 #include <iostream>
 
 using namespace sf;
 using namespace std;
+using namespace rapidjson;
+Document jsonReceiver(Packet packet)
+{
+    string pet;
+    Document petD;
+
+    packet >> pet;
+    cout << pet << endl;
+    const char* petChar = pet.c_str();
+    petD.Parse(petChar);
+
+    return petD;
+}
+
+string jsonSender(string memory, string value, string variable, string line)
+{
+    string jsonStr = "{\"memory\":"+ memory + ",\"value\":" + value + ",\"variable\":" + variable + ",\"line\":" + line + "}";
+    return jsonStr;
+}
 
 int main()
 {
     IpAddress ip = IpAddress::getLocalAddress();
     TcpSocket socket;
-    char connectionType, mode, buffer[2000];
+    char connectionType, mode;
+    bool done = false;
+    Packet packetR, packetS;
     size_t received;
-    string text = "Connected to: Server";
+    Document petition;
+    string json;
 
     TcpListener listener;
     listener.listen(8080);
     listener.accept(socket);
+    mode = 'r';
     
-    socket.send(text.c_str(), text.length() + 1);
-    socket.receive(buffer, sizeof(buffer), received);
-    cout << buffer << endl;
+    while (!done)
+    {
+        Event event;
+        if (event.key.code == Keyboard::Escape)
+            done = true;
+        
+        socket.receive(packetR);
+
+        if (packetR.getData() == NULL)
+            cout << "No message received yet" << endl;
+
+        else
+        {
+            petition = jsonReceiver(packetR);
+            Value& type = petition["type"];
+            Value& value = petition["value"];
+            Value& variable = petition["variable"];
+            Value& line = petition["line"];
+
+            if (type.GetString() == "int")
+            {
+                //Hace offset de 4 bytes
+            }
+            else if (type.GetString() == "long")
+            {
+                //Hace offset de 8 bytes
+            }
+            else if (type.GetString() == "char")
+            {
+                //Hace offset de 1 byte
+            }
+            else if (type.GetString() == "float")
+            {
+                //Hace offset de 4 bytes
+            }
+            else if (type.GetString() == "double")
+            {
+                //Hace offset de 8 bytes
+            }
+            else if (type.GetString() == "struct")
+            {
+                //Sigue sintaxis de C
+            }
+            else if (type.GetString() == "reference")
+            {
+                //Hace offset de 4 bytes
+            }
+
+            json = jsonSender("0x000", value.GetString(), variable.GetString(), line.GetString());
+            packetS << json;
+            socket.send(packetS);
+        }
+    }
     
-    system("pause");
     return 0;
 }
